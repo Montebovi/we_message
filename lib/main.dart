7e7184ffbab873eb89b131aa07c93f47471219ff
m.montebovi <m.montebovi@loccioni.com>
2022-01-24 19:30:26 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:we_message/screens/welcome_screen.dart';
+import 'package:we_message/screens/login_screen.dart';
+import 'package:we_message/screens/registration_screen.dart';
+import 'package:we_message/screens/chat_screen.dart';
+import 'package:we_message/screens/welcome_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+      },
+    );
+  }
+}
